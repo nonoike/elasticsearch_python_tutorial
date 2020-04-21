3ede757fe6b2242b6dd4d6b95b96f984f88aefb3
nonoike <ij4nu8d4fw@gmail.com>
2020-04-22 08:57:00 +0900

    add index.html

--- app/src/views.py
+++ app/src/views.py
@@ -1,10 +1,10 @@
 from elasticsearch import Elasticsearch
-from flask import request
+from flask import request, render_template
 
 from src import app
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def show_results():
     es = Elasticsearch([{'host': 'es01', 'port': '9200'}])
 
@@ -14,11 +14,13 @@ def show_results():
                 "must": []
             }
         },
-        "highlight": {
-            "fields": {
-                "hotelSpecial": {}
+        "sort": [
+            {
+                "hotelNo": {
+                    "order": "asc"
+                }
             }
-        }
+        ]
     }
 
     if search_word := request.form.get('search_word'):
@@ -26,22 +28,35 @@ def show_results():
             {
                 "bool": {
                     "should": [
-                        {"match": {"hotelName": request.form.get(search_word)}},
-                        {"match": {"hotelKanaName": request.form.get(search_word)}},
-                        {"match": {"hotelSpecial": request.form.get(search_word)}}
+                        {"match": {"hotelName": search_word}},
+                        {"match": {"hotelKanaName": search_word}},
+                        {"match": {"hotelSpecial": search_word}}
                     ]
                 }
             }
         )
+        body['highlight'] = {
+            "fields": {
+                "hotelName": {},
+                "hotelKanaName": {},
+                "hotelSpecial": {}
+            }
+        }
 
     if review_min := request.form.get('review_min'):
-        body['query']['bool']['must'].append({"range": {"reviewAverage": {"gte": request.get(review_min)}}})
+        body['query']['bool']['must'].append({"range": {"reviewAverage": {"gte": review_min}}})
 
     if review_max := request.form.get('review_max'):
-        body['query']['bool']['must'].append({"range": {"reviewAverage": {"gte": request.get(review_max)}}})
+        body['query']['bool']['must'].append({"range": {"reviewAverage": {"lte": review_max}}})
+
+    if charge_min := request.form.get('charge_min'):
+        body['query']['bool']['must'].append({"range": {"hotelMinCharge": {"gte": charge_min}}})
+
+    if charge_max := request.form.get('charge_max'):
+        body['query']['bool']['must'].append({"range": {"hotelMinCharge": {"lte": charge_max}}})
 
     result = es.search(index='hotel', body=body, size=1000)
     result_num = result['hits']['total']['value']
     hotels = result['hits']['hits']
 
-    return str(hotels)
+    return render_template('index.html', result_num=result_num, hotels=hotels, request_form=request.form)
